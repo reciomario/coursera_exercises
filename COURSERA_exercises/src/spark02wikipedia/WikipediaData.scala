1bc271bdb309d99395699b8f68103c0b5350745e
Mario Recio <reciomario@gmail.com>
2017-09-20 16:44:54 +0200

    Wikipedia Spark exercise commit

---
+++ COURSERA_exercises/src/spark02wikipedia/WikipediaData.scala
@@ -0,0 +1,20 @@
+package spark02wikipedia
+
+import java.io.File
+
+object WikipediaData {
+
+  private[spark02wikipedia] def filePath = {
+    val resource = this.getClass.getClassLoader.getResource("wikipedia/wikipedia.dat")
+   // if (resource == null) sys.error("Please download the dataset as explained in the assignment instructions")
+    new File("src/main/resources/wikipedia/wikipedia.dat").getPath
+  }
+
+  private[spark02wikipedia] def parse(line: String): WikipediaArticle = {
+    val subs = "</title><text>"
+    val i = line.indexOf(subs)
+    val title = line.substring(14, i)
+    val text  = line.substring(i + subs.length, line.length-16)
+    WikipediaArticle(title, text)
+  }
+}
